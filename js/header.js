906677954fa0381307de8c5e05a4fbe8ba9f4b99
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2015-02-09 08:50:42 +0100

    Cleared

--- js/header.js
+++
@@ -1,9 +0,0 @@
-var select = document.querySelector('.subheader');
-var initialPosition = $(select).offset().top
-addEventListener('scroll', function (event) {
-  if (window.scrollY > initialPosition) {
-    select.classList.add('docked');
-  } else if (select.classList.contains('docked')) {
-    select.classList.remove('docked');
-  }
-});
